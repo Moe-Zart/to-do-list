9a6793b248acd599d736057208c7a2787bb524d3
Moe-Zart <mohamedzreika03@outlook.com>
2022-08-04 23:52:06 +1000

    final commit

--- app.js
+++ app.js
@@ -3,31 +3,38 @@ let list = document.querySelector(".list");
 let todoList = [
   {
     id: 1,
-    task: "hi",
-  },
-  {
-    id: 2,
-    task: "bye",
+    task: "Create Your Todo List",
   },
 ];
-
-renderTodo()
-function todoTitle(event) {
-    console.log(event.target.value);
+let inputValue = "";
+function todoInput(event) {
+  inputValue = event.target.value;
 }
-function todoRemove(id){
-   todoList= todoList.filter((todo)=> {
-    return todo.id !== id})
-    renderTodo()
+function todoAdd() {
+  todoList.push({
+    id: Math.random() * 1000,
+    task: inputValue,
+  });
+  renderTodo();
 }
 
+function todoRemove(id) {
+  todoList = todoList.filter((todo) => {
+    return todo.id !== id;
+  });
+  renderTodo();
+}
+renderTodo();
 function renderTodo() {
-  list.innerHTML = todoList.map((todo) => 
-    `<li>
+  list.innerHTML = todoList
+    .map(
+      (todo) =>
+        `<li>
         ${todo.task}
         <button class="todo__delete" onclick="todoRemove(${todo.id})">
           x
         </button>
       </li>`
-  ).join('');
+    )
+    .join("");
 }
